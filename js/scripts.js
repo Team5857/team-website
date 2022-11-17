5137723e0b484eb87b35acfb3c0e3ed742ade08c
BetaTester41 <76068161+BetaTester41@users.noreply.github.com>
2022-11-17 11:23:03 -0800

    Merge scripts file, and remove unused code Yay load time :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,285 +1,102 @@
 /*!
-* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-});
-
-// Original source from here:
-// https://github.com/tholman/elevator.js
-/*!
- * Elevator.js
- *
- * MIT licensed
- * Copyright (C) 2015 Tim Holman, http://tholman.com
+ * Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
  */
 
-/*********************************************
- * Elevator.js
- *********************************************/
+function strap() {
+	// Navbar shrink function
+	var navbarShrink = function () {
+		const navbarCollapsible = document.body.querySelector("#mainNav");
+		if (!navbarCollapsible) {
+			return;
+		}
+		if (window.scrollY === 0) {
+			navbarCollapsible.classList.remove("navbar-shrink");
+		} else {
+			navbarCollapsible.classList.add("navbar-shrink");
+		}
+	};
 
- var Elevator = (function() {
+	// Shrink the navbar
+	navbarShrink();
 
-    'use strict';
+	// Shrink the navbar when page is scrolled
+	document.addEventListener("scroll", navbarShrink);
 
-    // Elements
-    var body = null;
+	// Collapse responsive navbar when toggler is visible
+	const navbarToggler = document.body.querySelector(".navbar-toggler");
+	const responsiveNavItems = [].slice.call(document.querySelectorAll("#navbarResponsive .nav-link"));
+	responsiveNavItems.map(function (responsiveNavItem) {
+		responsiveNavItem.addEventListener("click", () => {
+			if (window.getComputedStyle(navbarToggler).display !== "none") {
+				navbarToggler.click();
+			}
+		});
+	});
+}
 
-    // Scroll vars
-    var animation = null;
-    var duration = null; // ms
-    var customDuration = false;
-    var startTime = null;
-    var startPosition = null;
-    var elevating = false;
+// Script by birbbbbbb
 
-    var mainAudio;
-    var endAudio;
+// type 'birb' on your keyboard
 
-    /**
-     * Utils
-     */
+function birb() {
+	var key = [66, 73, 82, 66];
+	var ck = 0;
+	var max = key.length;
+	var onscreen = false;
 
-    // Soft object augmentation
-    function extend( target, source ) {
-        for ( var key in source ) {
-            if ( !( key in target ) ) {
-                target[ key ] = source[ key ];
-            }
-        }
-        return target;
-    };
+	var unicorn = function () {
+		var img = new Image();
+		img.src = data;
+		img.style.pointerEvents = "none";
+		img.style.width = "600px";
+		img.style.height = "338px";
+		img.style.transition = "13s all";
+		img.style.position = "fixed";
+		img.style.right = "-374px";
+		// img.style.bottom = 'calc(-50% + 280px)';
+		img.style.top = "100px";
+		img.style.zIndex = 999999;
 
-    // Thanks Mr Penner - http://robertpenner.com/easing/
-    function easeInOutQuad( t, b, c, d ) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-    };
+		document.body.appendChild(img);
 
-    function extendParameters(options, defaults){
-        for(var option in defaults){
-            var t = options[option] === undefined && typeof option !== "function";
-            if(t){
-                options[option] = defaults[option];
-            }
-        }
-        return options;
-    }
+		window.setTimeout(function () {
+			img.style.right = "calc(100% + 500px)";
+		}, 50);
 
-    /**
-     * Main
-     */
+		window.setTimeout(function () {
+			img.parentNode.removeChild(img);
+		}, 10300);
+		setTimeout(function () {
+			onscreen = false;
+		}, 10300);
+	};
 
-    // Time is passed through requestAnimationFrame, what a world!
-    function animateLoop( time ) {
-        if (!startTime) {
-            startTime = time;
-        }
+	var record = function (e) {
+		console.log(e.key, ck);
+		if (e.which === key[ck]) {
+			ck++;
+		} else {
+			ck = 0;
+		}
 
-        var timeSoFar = time - startTime;
-        var easedPosition = easeInOutQuad(timeSoFar, startPosition, -startPosition, duration);                        
-        
-        window.scrollTo(0, easedPosition);
+		if (ck >= max && !onscreen) {
+			onscreen = true;
+			unicorn();
+			ck = 0;
+		}
+	};
 
-        if( timeSoFar < duration ) {
-            animation = requestAnimationFrame(animateLoop);
-        } else {
-            animationFinished();
-        }
-   };
+	var init = function (data) {
+		document.addEventListener("keyup", record);
+	};
 
-//            ELEVATE!
-//              /
-//         ____
-//       .'    '=====<0
-//       |======|
-//       |======|
-//       [IIIIII[\--()
-//       |_______|
-//       C O O O D
-//      C O  O  O D
-//     C  O  O  O  D
-//     C__O__O__O__D
-//    [_____________]
-    function elevate() {
+	var data = "https://raw.githubusercontent.com/birbbbbbbie/birbbbbbbie.github.io/main/F99F9B71-C188-41FE-ABF1-5A383781363E.gif";
 
-        if( elevating ) {
-            return;
-        }
+	init(data);
+}
 
-        elevating = true;
-        startPosition = (document.documentElement.scrollTop || body.scrollTop);
-        
-        // No custom duration set, so we travel at pixels per millisecond. (0.75px per ms)
-        if( !customDuration ) {
-            duration = (startPosition * 1.5);
-        }
-
-        requestAnimationFrame( animateLoop );
-
-        // Start music!
-        if( mainAudio ) {
-            mainAudio.play();
-        }
-    }
-
-    function resetPositions() {
-        startTime = null;
-        startPosition = null;
-        elevating = false;
-    }
-
-    function animationFinished() {
-        
-        resetPositions();
-
-        // Stop music!
-        if( mainAudio ) {
-            mainAudio.pause();
-            mainAudio.currentTime = 0;
-        }
-
-        if( endAudio ) {
-            endAudio.play();
-        }
-    }
-
-    function onWindowBlur() {
-
-        // If animating, go straight to the top. And play no more music.
-        if( elevating ) {
-
-            cancelAnimationFrame( animation );
-            resetPositions();
-
-            if( mainAudio ) {
-                mainAudio.pause();
-                mainAudio.currentTime = 0;
-            }
-
-            window.scrollTo(0, 0);
-        }
-    }
-
-    //@TODO: Does this need tap bindings too?
-    function bindElevateToElement( element ) {
-        element.addEventListener('click', elevate, false);
-    }
-
-    function main( options ) {
-
-        // Bind to element click event, if need be.
-        body = document.body;
-
-        var defaults = {
-            duration: undefined,
-            mainAudio: false,
-            endAudio: false,
-            preloadAudio: true,
-            loopAudio: true,
-        };
-
-        options = extendParameters(options, defaults);
-        
-        if( options.element ) {
-            bindElevateToElement( options.element );
-        }
-
-        if( options.duration ) {
-            customDuration = true;
-            duration = options.duration;
-        }
-
-        window.addEventListener('blur', onWindowBlur, false);
-
-        // If the browser doesn't support audio, stop here!
-        if ( !window.Audio ) {
-            return;
-        }
-
-        if( options.mainAudio ) {
-            mainAudio = new Audio( options.mainAudio );
-            mainAudio.setAttribute( 'preload', options.preloadAudio ); 
-            mainAudio.setAttribute( 'loop', options.loopAudio );
-        }
-
-        if( options.endAudio ) {
-            endAudio = new Audio( options.endAudio );
-            endAudio.setAttribute( 'preload', 'true' );
-        }
-    }
-
-    return extend(main, {
-        elevate: elevate
-    });
-})();
-
-
-// trigger it
-
-var elementButton = document.querySelector('.elevator');
-var elevator = new Elevator({
-  element: elementButton,
-  // mainAudio from here:
-  // https://github.com/tholman/elevator.js
-  // Audio in the Demo (sourced from BenSound) is licenced under Creative Commons.
-  mainAudio: 'https://weichiachang.github.io/Easter-egg/images/music/elevator.mp3',
-  // endAudio from here:
-  // https://www.findsounds.com/ISAPI/search.dll?keywords=ding+dinging
-  endAudio:  'https://inventwithpython.com/pickup.wav'
-});
+//init all functions
+birb();
+strap();
